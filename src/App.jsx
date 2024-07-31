d921b989a0200ec05f9e8ff236015c134c8bbbb2
MPrashant1100 <prashantmishra1100@gmail.com>
2024-08-01 01:14:58 +0530

    Modification in calendar component

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,19 @@ function App() {
 
   return (
     <React.Fragment>
-      <Header
-        currentMonth={currentMonth}
-        currentYear={currentYear}
-        onPrevMonth={handlePrevMonth}
-        onNextMonth={handleNextMonth}
-      />
-      <Calendar currentMonth={currentMonth} currentYear={currentYear} />
+      <div className="relative">
+        <div className="fixed top-0 left-0 right-0 bg-white">
+          <Header
+            currentMonth={currentMonth}
+            currentYear={currentYear}
+            onPrevMonth={handlePrevMonth}
+            onNextMonth={handleNextMonth}
+          />
+        </div>
+        <div className="pt-14">
+          <Calendar currentMonth={currentMonth} currentYear={currentYear} />
+        </div>
+      </div>
     </React.Fragment>
   );
 }
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,8 +4,12 @@ import PropTypes from "prop-types";
 const Calendar = ({ currentMonth, currentYear }) => {
   const [days, setDays] = useState([]);
   const [currentDay, setCurrentDay] = useState(new Date().getDate());
-  const [currentMonthIndex, setCurrentMonthIndex] = useState(new Date().getMonth());
-  const [currentYearIndex, setCurrentYearIndex] = useState(new Date().getFullYear());
+  const [currentMonthIndex, setCurrentMonthIndex] = useState(
+    new Date().getMonth()
+  );
+  const [currentYearIndex, setCurrentYearIndex] = useState(
+    new Date().getFullYear()
+  );
 
   useEffect(() => {
     const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
@@ -29,13 +33,18 @@ const Calendar = ({ currentMonth, currentYear }) => {
       {days.map((day) => (
         <div
           key={day.date}
-          className={`p-2 ${
-            day.date === currentDay && currentMonth === currentMonthIndex && currentYear === currentYearIndex
+          className={`p-2 border ${
+            day.date === currentDay &&
+            currentMonth === currentMonthIndex &&
+            currentYear === currentYearIndex
               ? "text-white bg-blue-600 font-semibold border rounded-full"
               : ""
           }`}
         >
-          {day.date} {day.dayName}
+          <div className="flex gap-2 justify-center px-1">
+            <div> {day.date} </div>
+            <div> {day.dayName}</div>
+          </div>
         </div>
       ))}
     </div>
@@ -43,8 +52,8 @@ const Calendar = ({ currentMonth, currentYear }) => {
 };
 
 Calendar.propTypes = {
-    currentMonth: PropTypes.number.isRequired,
-    currentYear: PropTypes.number.isRequired,
-  };
+  currentMonth: PropTypes.number.isRequired,
+  currentYear: PropTypes.number.isRequired,
+};
 
 export default Calendar;
